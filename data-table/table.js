46f5eab3c6cbf377e8585430fd43e786c244e4b6
brmighell <70190053+brmighell@users.noreply.github.com>
2021-10-12 19:38:56 -0700

    HTML example working, human-friendly cell entries, refactored existing code

--- data-table/table.js
+++ data-table/table.js
@@ -1,133 +1,90 @@
-/*
+/**
  * Global vars - storing data for callbacks
  */
-
-/**
- * Global variable containing configuration information for the table
- * @property {string} wrapperDivId          - ID for the wrapper
- * @property {number} numRows               - Number of rows in the table
- * @property {number} numColumns            - Number of columns in the table
- * @property {object} cellFieldNames        - Array of the names for all fields in a cell
- * @property {object} cellFieldTypes        - Array of the types for all fields in a cell
- * @property {object} cellFieldDefaults     - Array of the defaults for all fields in a cell
- * @property {object} cellFieldCallbacks    - Array of the callbacks for all fields in a cell
- */
-// let config = null;
-
-/**
- * @property {string} wrapperDivId      - ID for the div passed in by the client
- * @property {string} tableDivId        - ID for the div containing the table
- * @property {string} entryBoxDivId     - ID for the div containing the entry box
- * @property {string} tableElementId    - Id for the entire table element
- * @property {string} theadElementId    - ID for the table's first row (column headers) element
- * @property {string} tbodyElementId    - ID for the table's body element
- * @type {null}
- */
-// let tableIds = null;
-
-/**
- * @property {object} Names     - Array of the names for all fields in a cell
- * @property {object} Types     - Array of the types for all fields in a cell
- * @property {object} Defaults  - Array of the defaults for all fields in a cell
- * @property {object} Callbacks - Tells what function, if any, to execute when a field is changed
- * @type {null}
- */
-// let datumConfig = null;
-
 /* Collection of eventTypes
 * TODO: Figure out why lint doesn't like ENUMs */
 // enum eventTypes = {};
+
+// Initialization of config dictionary; stores in the form {wrapperDivId : config}
+let configDict = {};
+
+/**
+ * Config object for each datatable. Expects clientConfig.wrapperDivId to exist.
+ */
+class Config {
+    /**
+     * Global variable containing configuration information for the table
+     * @param {object} clientConfig     - Configuration information passed in by the client
+     * @property {string} wrapperDivId  - ID for the wrapper
+     * @property {number} numRows       - Number of rows in the table
+     * @property {number} numColumns    - Number of columns in the table
+     * @property {object} tableIds      - Container for all the table's magic strings
+     * @property {object} datumConfig   - Container for default values of a cell
+     */
+    constructor(clientConfig) {
+        this.numRows = clientConfig.numRows === undefined ? 3 : clientConfig.numRows;
+        this.numColumns = clientConfig.numColumns === undefined ? 3 : clientConfig.numColumns;
+
+        /**
+         * @property {string} wrapperDivId      - ID for the div passed in by the client
+         * @property {string} tableDivId        - ID for the div containing the table
+         * @property {string} entryBoxDivId     - ID for the div containing the entry box
+         * @property {string} tableElementId    - Id for the entire table element
+         * @property {string} theadElementId    - ID for the table's first row (column headers) element
+         * @property {string} tbodyElementId    - ID for the table's body element
+         */
+        this.tableIds = {
+            wrapperDivId: clientConfig.wrapperDivId,
+            tableDivId: '_tableDivId_' + clientConfig.wrapperDivId,
+            entryBoxDivId: '_entryBoxDivId_' + clientConfig.wrapperDivId,
+            tableElementId: '_tableId_' + clientConfig.wrapperDivId,
+            theadElementId: '_theadId_' + clientConfig.wrapperDivId,
+            tbodyElementId: '_tbodyId_' + clientConfig.wrapperDivId
+        }
+
+        /**
+         * @property {object} names     - Array of the names for all fields in a cell | Default: ["Value"]
+         * @property {object} types     - Array of the types for all fields in a cell | Default: [Number]
+         * @property {object} defaults  - Array of the defaults for all fields in a cell | Default: [0]
+         * @property {object} callbacks - Tells what function to execute when a field is changed | Default: ["None"]
+         */
+        this.datumConfig = {
+            names: clientConfig.names === undefined ? ["Value"] : clientConfig.names,
+            types: clientConfig.types === undefined ? [Number] : clientConfig.types,
+            values: clientConfig.values === undefined ? [0] : clientConfig.values,
+            callbacks: clientConfig.callbacks === undefined ? ["None"] : clientConfig.callbacks
+        }
+    }
+}
 
 /**
  * Functions for table creation
  */
 
 /**
- * Creates a unique table div ID given the wrapper div ID
- * @param {string} wrapper ID for the wrapper of the table
- * @returns {object} Returns object containing useful IDs
- */
-function convertWrapperDivIdToTableIds(wrapper) {
-    let tableIds = {
-        wrapperDivId: wrapper,
-        tableDivId: '_tableDivId_' + wrapper,
-        entryBoxDivId: '_entryBoxDivId_' + wrapper,
-        tableElementId: '_tableId_' + wrapper,
-        theadElementId: '_theadId_' + wrapper,
-        tbodyElementId: '_tbodyId_' + wrapper
-    }
-
-    return tableIds;
-}
-
-function createDatumConfig() {
-    let datumConfig = {
-        Names: ["Votes", "Status"],
-        Types: [Number, []],
-        Values: [0, ["Active", "Elected", "Eliminated"]],
-        Callbacks: []
-    }
-
-    return datumConfig;
-}
-
-/**
  * If any option is not provided, chooses a sane default
- * @param {object} clientConfig A set of overriding config values, edits in-place
- * @throws Error if any required option is not provided
- * @returns {object} Returns config information
+ * @param {object} clientConfig - A set of overriding config values, edits in-place
+ * @returns {undefined}         - Doesn't return anything
+ * @throws Error                - If any required option is not provided
  */
-function setConfigDefaults(clientConfig) {
-
-    let config = {
-        numRows: 3,
-        numColumns: 3,
-        tableIds: null,
-        datumConfig: null
-    };
+function setConfig(clientConfig) {
 
     if (clientConfig.wrapperDivId === undefined) {
         throw new Error("An ID for the wrapper div is required");
     }
 
-    config.tableIds = convertWrapperDivIdToTableIds(clientConfig.id);
-
-    if (clientConfig.numRows !== undefined) {
-        config.numRows = clientConfig.numRows;
-    }
-
-    if (clientConfig.numColumns !== undefined) {
-        config.numColumns = clientConfig.numColumns;
-    }
-
-    config.datumConfig = createDatumConfig();
-
-    if (clientConfig.cellFieldNames !== undefined) {
-        config.datumConfig.Names = ["Votes", "Status"];
-    }
-
-    if (clientConfig.cellFieldTypes !== undefined) {
-        config.datumConfig.Types = [Number, []];
-    }
-
-    if (clientConfig.cellFieldDefaults !== undefined) {
-        config.datumConfig.Values = [0, ["Active", "Elected", "Eliminated"]];
-    }
-
-    if (clientConfig.cellFieldCallbacks !== undefined) {
-        config.datumConfig.Callbacks = [];
-    }
+    let config = new Config(clientConfig);
 
     validateConfig(config);
 
-    return config;
+    configDict[clientConfig.wrapperDivId] = config;
 }
 
 /**
  * Performs basic logic-checking on the config after defaults have been set
- * @param {object} config configuration file
- * @throws Error if any of the components don't make sense
- * @returns {undefined} Returns absolutely nothing
+ * @param {object} config   - Table configuration object
+ * @returns {undefined}     - Doesn't return anything
+ * @throws Error            - If any required option is not provided
  */
 function validateConfig(config) {
 
@@ -136,18 +93,11 @@ function validateConfig(config) {
     }
 }
 
-function createEntryCell(indexString) {
-    /**
-     * Accesses the config to figure out what to make, then creates
-     * a cell at the specified spot in the HTML
-     * TODO: Fill this out
-     */
-}
-
-function cellIndexToElementId(rowIndex, colIndex) {
-    return "_row_" + rowIndex + "_and_col_" + colIndex + "_";
-}
-
+/**
+ * Creates two sub divs and inserts them into the wrapper div
+ * @param {object} config   - Table configuration object
+ * @returns {undefined}     - Doesn't return anything
+ */
 function createSubDivs(config) {
     let entryBoxDiv = document.createElement("div");
     entryBoxDiv.id = config.tableIds.entryBoxDivId;
@@ -155,7 +105,6 @@ function createSubDivs(config) {
     let tableDiv = document.createElement("div");
     tableDiv.id = config.tableIds.tableDivId;
 
-    // FIX: Delete this!
     let text = document.createTextNode("~~This is where the entry box will live~~");
     entryBoxDiv.appendChild(text);
 
@@ -164,9 +113,9 @@ function createSubDivs(config) {
 }
 
 /**
- * Creates an HTML table.
- * Assigns unique element IDs to all parts of the table.
- * @returns {undefined} Returns absolutely nothing
+ * Creates an HTML table and assigns unique element IDs to all parts of the table.
+ * @param {object} config   - Table configuration object
+ * @returns {undefined}     - Doesn't return anything
  */
 function createTable(config) {
     let table = document.createElement("table");
@@ -178,11 +127,11 @@ function createTable(config) {
     let tbody = document.createElement("tbody");
     tbody.id = config.tableIds.tbodyElementId;
 
-    addRows(config, config.numRows, 0)
-
     table.appendChild(tbody);
     document.getElementById(config.tableIds.tableDivId).appendChild(table);
     document.getElementById(config.tableIds.wrapperDivId).appendChild(document.getElementById(config.tableIds.tableDivId))
+
+    addRows(config, config.numRows, 0)
 }
 
 function createColumnEntryBox() {
@@ -242,31 +191,72 @@ function createRowEntryBox() {
 
 /**
  * Adds rows to the table.
- * @param {Number} numberOfRows Number of rows to be added
- * @param {Number} index Vertical index where to start adding rows
- * @returns {undefined} Doesn't return anything
+ * @param {object} config       - Table configuration object
+ * @param {Number} numberOfRows - Number of rows to be added
+ * @param {Number} index        - Vertical index where to start adding rows
+ * @returns {undefined}         - Doesn't return anything
  */
 function addRows(config, numberOfRows, index) {
     // For each of the newly requested rows
     for (let newRow = 0; newRow < numberOfRows; newRow++) {
-        // Insert a row into the body of the table
-        let row = document.getElementById(config.tableIds.tbodyElementId).insertRow(index + newRow);
-        // Then create a cell for each column
-        for (let colIndex = 0; colIndex < config.numColumns; colIndex++) {
-            let cell = row.insertCell(colIndex);
-            let indexString = cellIndexToElementId(index + newRow, colIndex)
-            let text = document.createTextNode(indexString);
-            cell.appendChild(text);
-        }
+        addSingleRow(config, index + newRow)
     }
 }
 
-function addSingleRow(index) {
+/**
+ * Adds a single row to the table
+ * @param {object} config   - Table configuration object
+ * @param {Number} rowIndex - Where to insert the row
+ * @returns {undefined}     - Doesn't return anything
+ */
+function addSingleRow(config, rowIndex) {
+    // Insert a row into the body of the table
+    let row = document.getElementById(config.tableIds.tbodyElementId).insertRow(rowIndex);
+    // Then for each column
+    for (let colIndex = 0; colIndex < config.numColumns; colIndex++) {
+        // Create an entry cell
+        createEntryCell(config, row, rowIndex, colIndex)
+    }
+}
+
+/**
+ * Adds a cell to a specific index on a passed-in row
+ * @param {object} config           - Table configuration object
+ * @param {HTMLTableRowElement} row - HTML table element to which the cell will be added
+ * @param {Number} rowIndex         - The index of the row, used to create the cell's magic string
+ * @param {Number} colIndex         - The index of the column, used to create the cell's magic string
+ * @returns {undefined}             - Doesn't return anything
+ */
+function createEntryCell(config, row, rowIndex, colIndex) {
     /**
-     * Adds single row from existing table. Will probably call
-     * createEntryCell(). Maybe should also accept index as an argument?
-     * TODO: Fill this out
+     * TODO: Set this up to deal with default fields
      */
+
+    let cell = row.insertCell(colIndex);
+    cell.id = cellIndexToElementId(config.wrapperDivId, rowIndex, colIndex)
+
+    let top = document.createTextNode("---------");
+    let br1 = document.createElement("br");
+    let middle = document.createTextNode("| (" + rowIndex + ", " + colIndex + ") |");
+    let br2 = document.createElement("br");
+    let bottom = document.createTextNode("---------");
+
+    cell.appendChild(top);
+    cell.appendChild(br1);
+    cell.appendChild(middle);
+    cell.appendChild(br2);
+    cell.appendChild(bottom);
+}
+
+/**
+ * Creates a magic string for a cell
+ * @param wrapperDivId  - ID for the wrapper
+ * @param rowIndex      - The row index for a cell
+ * @param colIndex      - The column index for a cell
+ * @returns {string}    - Returns a magic string unique to a cell, based on location
+ */
+function cellIndexToElementId(wrapperDivId, rowIndex, colIndex) {
+    return wrapperDivId + "row_" + rowIndex + "_and_col_" + colIndex + "_";
 }
 
 function deleteRows(numberOfRows, index) {
@@ -335,23 +325,21 @@ function hideHelpTooltip() {
  * Public functions below
  */
 
+/**
+ * Function available to client in order to create a datatable
+ * @param {object} clientConfig - Client configuration requests
+ */
 // eslint-disable-next-line no-unused-vars,camelcase
 function dt_CreateDataTable(clientConfig) {
     /**
      * Creates the HTML data table
      * TODO: Finish filling this out
-     * TODO: Transfer code to appropriate functions
-     * TODO: Make sure this works
      */
+    setConfig(clientConfig);
 
-    let config = setConfigDefaults(clientConfig);
+    createSubDivs(configDict[clientConfig.wrapperDivId]);
 
-    createSubDivs(config);
-
-    createTable(config);
-
-    let element = document.getElementById(clientConfig.wrapperDivId);
-    element.innerHTML += "This is a test";
+    createTable(configDict[clientConfig.wrapperDivId]);
 }
 
 function dt_disableField(row, col, fieldName) {
@@ -401,6 +389,6 @@ function dt_getFieldValue(row, col, fieldName, value) {
 
 // In case of node.js
 /* eslint no-undef: ["off"] */
-// if (typeof exports !== typeof undefined) {
-//     exports.createDataTable = dt_createDataTable;
-// }
+if (typeof exports !== typeof undefined) {
+    exports.createDataTable = dt_createDataTable;
+}
